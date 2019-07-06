1a8c09a84d0f9fe545dc9449beacadd321736732
Jonathan Nuñez <jonathan.ntorres@alumnos.udg.mx>
2019-07-05 22:47:34 -0500

    Variables
    Clase 1 de Fundamentos de JS: Variables

---
+++ clase 1.js
@@ -0,0 +1,11 @@
+var nombre = "Jonathan",
+  apellido = "Nuñez";
+
+var edad = 21;
+
+edad = "21 años";
+
+console.log("Hola " + nombre + " " + apellido);
+console.log("Tengo " + edad);
+
+var peso = 80;
