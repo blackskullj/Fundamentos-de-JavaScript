3ae8164b70bddae65613c62098969639883576b9
Jonathan Nuñez <jonathan.ntorres@alumnos.udg.mx>
2019-07-15 16:24:15 -0500

    Functions
    Clase 4 de Fundamentos de JS: Funciones

---
+++ clase 4.js
@@ -0,0 +1,11 @@
+var nombre = "Jonathan",
+  edad = 20;
+
+function imprimirEdad(n, e) {
+  console.log(`${n} tiene ${e} años`);
+}
+
+imprimirEdad(nombre, edad);
+imprimirEdad("Vicky", 28);
+imprimirEdad("Eric", 27);
+imprimirEdad("Darío", 24);
