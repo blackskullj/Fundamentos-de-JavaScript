22771a95d4015116e34297de4294119ba3ecbcb2
Jonathan Nuñez <jonathan.ntorres@alumnos.udg.mx>
2019-07-22 21:33:39 -0500

    Unstructing Objects
    Clase 7 de Fundamentos de JS: Desestructurando objetos

---
+++ clase 7.js
@@ -0,0 +1,28 @@
+var jonathan = {
+  nombre: "Jonathan",
+  apellido: "Nuñez",
+  edad: 20
+};
+
+var dario = {
+  nombre: "Darío",
+  apellido: "Susnisky",
+  edad: 27
+};
+
+function imprimirNombreEnMayusculas(persona) {
+  var { nombre } = persona;
+  console.log(nombre.toUpperCase());
+}
+
+imprimirNombreEnMayusculas(jonathan);
+imprimirNombreEnMayusculas(dario);
+imprimirNombreEnMayusculas({ nombre: "Pepito" });
+
+function imprimirNombreYEdad(persona) {
+  var { nombre } = persona;
+  var { edad } = persona;
+  console.log(`Hola, me llamo ${nombre} y tengo ${edad} años`);
+}
+
+imprimirNombreYEdad(jonathan);
