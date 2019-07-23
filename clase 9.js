e4c9e02c3a2cf770525bd746ca673df8ebef1c04
Jonathan Nuñez <jonathan.ntorres@alumnos.udg.mx>
2019-07-22 21:35:10 -0500

    Comparisions
    Clase 9 de Fundamentos de JS: Comparaciones

---
+++ clase 9.js
@@ -0,0 +1,16 @@
+var x = 4,
+  y = "4";
+
+var jonathan = {
+  nombre: "Jonathan"
+};
+
+/*var otraPersona = {
+  nombre: "Jonathan"
+};*/
+
+// var otraPersona = jonathan;
+
+var otraPersona = {
+  ...jonathan
+};
