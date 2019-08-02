070ac11d648de5dc91540b54643edfbefdd05ca3
Jonathan Nuñez <jonathan.ntorres@alumnos.udg.mx>
2019-08-02 17:41:36 -0500

    Class
    Clase 24 de Fundamentos de JS: La Verdad Oculta de las Clases

---
+++ clase 24.js
@@ -0,0 +1,37 @@
+function HeredaDe(prototipoHijo, prototipoPadre) {
+  var fn = function() {};
+  fn.prototype = prototipoPadre.prototype;
+  prototipoHijo.prototype = new fn();
+  prototipoHijo.prototype.constructor = prototipoHijo;
+}
+
+function Persona(nombre, apellido, altura) {
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.altura = altura;
+}
+
+Persona.prototype.saludar = function() {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
+};
+
+Persona.prototype.soyAlto = function() {
+  return this.altura > 1.8;
+};
+
+function Desarrollador(nombre, apellido) {
+  this.nombre = nombre;
+  this.apellido = apellido;
+}
+
+HeredaDe(Desarrollador, Persona);
+
+Desarrollador.prototype.saludar = function() {
+  console.log(
+    `Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador/a`
+  );
+};
+
+// var jonathan = new Persona("Jonathan", "Nuñez", 1.72);
+// var erika = new Persona("Erika", "Luna", 1.65);
+// var arturo = new Persona("Arturo", "Martinez", 1.89);
