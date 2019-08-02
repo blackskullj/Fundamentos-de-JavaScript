14a47085bed5f3244293cb760005709a16079036
Jonathan Nuñez <jonathan.ntorres@alumnos.udg.mx>
2019-08-02 17:41:04 -0500

    This: Arrow Functions
    Clase 23 de Fundamentos de JS: ¿Quién es THIS en las arrow functions?"

---
+++ clase 23.js
@@ -0,0 +1,17 @@
+function Persona(nombre, apellido, altura) {
+  this.nombre = nombre;
+  this.apelldio = apellido;
+  this.altura = altura;
+}
+
+Persona.prototype.saludar = function() {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apelldio}`);
+};
+
+Persona.prototype.soyAlto = function() {
+  return this.altura > 1.8;
+};
+
+var jonathan = new Persona("Jonathan", "Nuñez", 1.72);
+var erika = new Persona("Erika", "Luna", 1.65);
+var arturo = new Persona("Arturo", "Martinez", 1.89);
