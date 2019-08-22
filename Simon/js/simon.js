4b16e23475c2fe55c49691457ddef3e6cb5c6212
Jonathan Nuñez <jonathan.ntorres@alumnos.udg.mx>
2019-08-22 10:49:18 -0500

    Color Verification
    Verificar que el color elegido sea el correcto

--- Simon/js/simon.js
+++ Simon/js/simon.js
@@ -3,15 +3,19 @@ const violeta = document.getElementById("violeta");
 const naranja = document.getElementById("naranja");
 const verde = document.getElementById("verde");
 const btnEmpezar = document.getElementById("btnEmpezar");
+const ULTIMO_NIVEL = 10;
+
+swal("Hola");
 
 class Juego {
   constructor() {
     this.inicializar();
     this.generarSecuencia();
-    this.siguienteNIvel();
+    setTimeout(this.siguienteNIvel, 500);
   }
 
   inicializar() {
+    this.siguienteNIvel = this.siguienteNIvel.bind(this);
     this.elegirColor = this.elegirColor.bind(this);
     btnEmpezar.classList.add("hide");
     this.nivel = 1;
@@ -24,12 +28,13 @@ class Juego {
   }
 
   generarSecuencia() {
-    this.secuencia = new Array(10)
+    this.secuencia = new Array(ULTIMO_NIVEL)
       .fill(0)
       .map(n => Math.floor(Math.random() * 4));
   }
 
   siguienteNIvel() {
+    this.subnivel = 0;
     this.iluminarSecuencia();
     this.agregarEventosClick();
   }
@@ -44,6 +49,19 @@ class Juego {
         return "naranja";
       case 3:
         return "verde";
+    }
+  }
+
+  transformarColorANumero(color) {
+    switch (color) {
+      case "celeste":
+        return 0;
+      case "violeta":
+        return 1;
+      case "naranja":
+        return 2;
+      case "verde":
+        return 3;
     }
   }
 
@@ -70,8 +88,31 @@ class Juego {
     this.colores.naranja.addEventListener("click", this.elegirColor);
   }
 
+  eliminarEventosClick() {
+    this.colores.celeste.removeEventListener("click", this.elegirColor);
+    this.colores.verde.removeEventListener("click", this.elegirColor);
+    this.colores.violeta.removeEventListener("click", this.elegirColor);
+    this.colores.naranja.removeEventListener("click", this.elegirColor);
+  }
+
   elegirColor(ev) {
-    console.log(this);
+    const nombreColor = ev.target.dataset.color;
+    const numeroColor = this.transformarColorANumero(nombreColor);
+    this.iluminarColor(nombreColor);
+    if (numeroColor === this.secuencia[this.subnivel]) {
+      this.subnivel++;
+      if (this.subnivel === this.nivel) {
+        this.nivel++;
+        this.eliminarEventosClick();
+        if (this.nivel === ULTIMO_NIVEL + 1) {
+          //Ganó!
+        } else {
+          setTimeout(this.siguienteNIvel, 1500);
+        }
+      }
+    } else {
+      //Perdió
+    }
   }
 }
 
